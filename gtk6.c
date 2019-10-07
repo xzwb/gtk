59c861135e2f535d40da3391fb4adbabe86496b3
xzwb <1478863479@qq.com>
2019-10-07 21:42:48 +0800

    学习gtk

--- gtk6.c
+++ gtk6.c
@@ -1,9 +1,9 @@
-// 表格布局
+// 固定布局
 
 #include <stdio.h>
 #include <gtk/gtk.h>
 
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
     GtkWidget *window;
     GtkWidget *button;
@@ -19,18 +19,13 @@ int main(int argc, char *argv[])
     button1 = gtk_button_new_with_label("hello1");
     button2 = gtk_button_new_with_label("hello2");
     button3 = gtk_button_new_with_label("hello3");
-    // 创建表格布局容器
-    GtkWidget *table = gtk_table_new(2, 2, TRUE);
-    // 将创建的表格布局加到Window里
-    gtk_container_add((GtkContainer *)window, table);
-    // 将按钮加到表格布局里这时候不再用gtk_container_add
-    gtk_table_attach_defaults((GtkTable *)table, button, 0, 1, 0, 1);
-    gtk_table_attach_defaults((GtkTable *)table, button1, 1, 2, 0, 1);
-    gtk_table_attach_defaults((GtkTable *)table, button2, 0, 1, 1, 2);
-    gtk_table_attach_defaults((GtkTable *)table, button3, 1, 2, 1, 2);
-
+    // 创建固定布局
+    GtkWidget *fixed = gtk_fixed_new();
+    gtk_container_add((GtkContainer *)window, fixed);
+    gtk_fixed_put((GtkFixed *)fixed, button, 100, 100);
+    // 设置控件大小
+    gtk_widget_set_size_request(button, 100, 100);
     gtk_widget_show_all(window);
 
     gtk_main();
-
 }
